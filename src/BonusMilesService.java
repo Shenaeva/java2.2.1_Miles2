20861b37413f407c86dc02486deb3c0bfcdeebdf
Shenaeva <56427199+Shenaeva@users.noreply.github.com>
2021-02-06 19:48:34 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int price) {
+    public int calculate(int cost) {
         int milesPrice = 20;
-        int miles = price / milesPrice;
+        int miles = cost / milesPrice;
         return miles;
     }
 }
