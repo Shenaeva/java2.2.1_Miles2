98d5369b1dda658b922b23c32d5f340bfcee15c2
Olga Shenaeva <ovshenaeva@gmail.com>
2021-02-06 19:42:56 +0300

    Правки

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 30021;
-        int milePrice = 20;
-        int accruedMiles = ticketPrice / milePrice;
-        System.out.println(accruedMiles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 12_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
