d236ba47f93086040dc1a0e627ad18930ed6db5a
Olga Shenaeva <ovshenaeva@gmail.com>
2021-02-06 19:25:11 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 30021;
+        int milePrice = 20;
+        int accruedMiles = ticketPrice / milePrice;
+        System.out.println(accruedMiles);
+    }
+}
